5091eaf21d70159b55008d193c20cc4f773bb0ca
TheSummerKitten <14536971+thesummerkitten@user.noreply.gitee.com>
2025-03-02 00:11:12 +0800

    规则校验应用

--- kittenGLM-domain/src/main/java/com/kitten/domain/openai/service/AbstractChatService.java
+++ kittenGLM-domain/src/main/java/com/kitten/domain/openai/service/AbstractChatService.java
@@ -2,9 +2,12 @@ package com.kitten.domain.openai.service;
 
 import com.kitten.dataTypes.enums.OpenAiChannel;
 import com.kitten.domain.openai.model.aggregates.ChatProcessAggregate;
+import com.kitten.domain.openai.model.entity.RuleLogicEntity;
+import com.kitten.domain.openai.model.valobj.LogicCheckTypeVO;
 import com.kitten.domain.openai.service.channel.OpenAiGroupService;
 import com.kitten.domain.openai.service.channel.impl.ChatGLMService;
 import com.kitten.domain.openai.service.channel.impl.ChatGPTService;
+import com.kitten.domain.openai.service.rule.factory.DefaultLogicFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
@@ -32,6 +35,17 @@ public abstract class AbstractChatService implements IChatService {
         });
         emitter.onError(throwable -> log.error("流式问答请求疫情，使用模型：{}", chatProcess.getModel(), throwable));
 
+        // 2. 规则过滤
+        RuleLogicEntity<ChatProcessAggregate> ruleLogicEntity = this.doCheckLogic(chatProcess,
+                DefaultLogicFactory.LogicModel.ACCESS_LIMIT.getCode(),
+                DefaultLogicFactory.LogicModel.SENSITIVE_WORD.getCode());
+        if (!LogicCheckTypeVO.SUCCESS.equals(ruleLogicEntity.getType())) {
+            emitter.send(ruleLogicEntity.getInfo());
+            emitter.complete();
+            return emitter;
+        }
+
+
         // 3. 应答处理
         log.info("开始处理请求");
         openAiGroup.get(chatProcess.getChannel()).doMessageResponse(chatProcess, emitter);
@@ -39,6 +53,9 @@ public abstract class AbstractChatService implements IChatService {
         // 4. 返回结果
         return emitter;
     }
+
+    protected abstract RuleLogicEntity<ChatProcessAggregate> doCheckLogic(ChatProcessAggregate chatProcess, String... logics) throws Exception;
+
 
 
 }
--- kittenGLM-domain/src/main/java/com/kitten/domain/openai/service/ChatService.java
+++ kittenGLM-domain/src/main/java/com/kitten/domain/openai/service/ChatService.java
@@ -7,8 +7,12 @@ import com.kitten.chatglmsdk.session.OpenAiSession;
 import com.kitten.dataTypes.enums.ChatGLMModel;
 import com.kitten.dataTypes.exception.ChatGPTException;
 import com.kitten.domain.openai.model.aggregates.ChatProcessAggregate;
+import com.kitten.domain.openai.model.entity.RuleLogicEntity;
+import com.kitten.domain.openai.model.valobj.LogicCheckTypeVO;
 import com.kitten.domain.openai.service.channel.impl.ChatGLMService;
 import com.kitten.domain.openai.service.channel.impl.ChatGPTService;
+import com.kitten.domain.openai.service.rule.ILogicFilter;
+import com.kitten.domain.openai.service.rule.factory.DefaultLogicFactory;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.sse.EventSource;
 import okhttp3.sse.EventSourceListener;
@@ -20,14 +24,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class ChatService extends AbstractChatService {
 
-
-
+    @Resource
+    private DefaultLogicFactory logicFactory;
 
     public ChatService(ChatGPTService chatGPTService, ChatGLMService chatGLMService) {
         super(chatGPTService, chatGLMService);
@@ -35,56 +40,16 @@ public class ChatService extends AbstractChatService {
     }
 
 
-    //    @Resource(name = "chatGLMOpenAiSession")
-//    protected OpenAiSession chatGLMOpenAiSession;
-//    @Override
-//    protected void doMessageResponse(ChatProcessAggregate chatProcess, ResponseBodyEmitter emitter) throws Exception {
-//        //1. 请求消息
-//        List<ChatCompletionRequest.Prompt> prompts = chatProcess.getMessages().stream()
-//                .map(entity -> ChatCompletionRequest.Prompt.builder()
-//                        .role(Role.user.getCode())
-//                        .content(entity.getContent())
-//                        .build()
-//                )
-//                .collect(Collectors.toList());
-//
-//        //2. 封装参数
-//        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
-//                .topP(0.7F)
-//                .sseFormat("data")
-//                .stream(true)
-//                .incremental(false)
-//                .isCompatible(true)
-//                .temperature(0.9F)
-//                .model(Model.valueOf(ChatGLMModel.get(chatProcess.getModel()).name()))
-//                .prompt(prompts)
-//                .build();
-//        log.info("[请求参数] {}", JSON.toJSONString(chatCompletionRequest));
-//        //3. 请求应答
-//        chatGLMOpenAiSession.completions(chatCompletionRequest, new EventSourceListener() {
-//            @Override
-//            public void onEvent(@NotNull EventSource eventSource, @Nullable String id, @Nullable String type, @NotNull String data) {
-//                ChatCompletionResponse response = JSON.parseObject(data, ChatCompletionResponse.class);
-//                // 消息类型: add 增量，finish 结束，error 错误，interrupted 中断
-//                if (EventType.add.getCode().equals(type)){
-//                    try {
-//                        emitter.send(response.getData());
-//                        log.info("[输出增量] {}", JSON.toJSONString(response.getData()));
-//                    } catch (Exception e) {
-//                        throw new ChatGPTException(e.getMessage());
-//                    }
-//                }
-//                if (EventType.finish.getCode().equals(type)) {
-//                    ChatCompletionResponse.Meta meta = JSON.parseObject(response.getMeta(), ChatCompletionResponse.Meta.class);
-//                    log.info("[输出结束] Tokens {}", JSON.toJSONString(meta));
-//                }
-//            }
-//            // 结束钩子
-//            @Override
-//            public void onClosed(EventSource eventSource) {
-//                log.info("执行结束钩子");
-//                emitter.complete();
-//            }
-//        });
-//    }
+    // 子类实现父类抽象方法
+    @Override
+    protected RuleLogicEntity<ChatProcessAggregate> doCheckLogic(ChatProcessAggregate chatProcess, String... logics) throws Exception {
+        Map<String, ILogicFilter> logicFilterMap = logicFactory.openLogicFilter();
+        RuleLogicEntity<ChatProcessAggregate> entity = null;
+        for (String code : logics) {
+            entity = logicFilterMap.get(code).filter(chatProcess);
+            if (!LogicCheckTypeVO.SUCCESS.equals(entity.getType())) return entity;
+        }
+        return entity != null ? entity : RuleLogicEntity.<ChatProcessAggregate>builder()
+                .type(LogicCheckTypeVO.SUCCESS).data(chatProcess).build();
+    }
 }
