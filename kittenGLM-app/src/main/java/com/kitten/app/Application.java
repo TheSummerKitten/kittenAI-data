7ecc97accfb373beb177950b62ea629a0fb13da2
TheSummerKitten <14536971+thesummerkitten@user.noreply.gitee.com>
2025-03-02 15:48:18 +0800

    数据库配置

--- kittenGLM-app/src/main/java/com/kitten/app/Application.java
+++ kittenGLM-app/src/main/java/com/kitten/app/Application.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan({"com.kitten.trigger", "com.kitten.domain", "com.kitten.app"})
 @Configurable
 public class Application {
     public static void main(String[] args) {
